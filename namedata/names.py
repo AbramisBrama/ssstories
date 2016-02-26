334c1042918060c899b639cb56651c48f629fb64
saver_is_not <saver_is_not@bk.ru>
2016-02-26 15:14:16 -0500

    Names data draft

---
+++ namedata/names.py
@@ -0,0 +1,20 @@
+suffixes={
+  
+}
+
+names={
+  "Владимир": {
+    "Вов": {
+      1,
+      "к": 2,
+      "ан": 3
+    },
+    "Волод": {
+      
+    }
+  }
+}
+
+print names
+print names["Владимир"]
+print "Jkewe"            
