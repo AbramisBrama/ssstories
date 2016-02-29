8093599c1339e952c5ff8566bfe9ef6994f61e9e
Dmitry Myasnikov <saver_is_not@bk.ru>
2016-02-29 23:40:53 +0300

    Analyser quickfix

--- text/analyser.py
+++ text/analyser.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*-
+
 from namedata import names
 from namedata import suffixes
 
-
 def isName(word):
     rezult = False
-    for nameMap in names:
+    for nameMap in names.names:
         for key in nameMap:
             if word.startswith(key):
                 rezult = True
@@ -17,7 +18,7 @@ def isName(word):
             if len(subsuff) > 0 and (len(word) - len(key) > 2):  # suffix found
                 if word.startswith(key+subsuff):
                     rezult = True
-                    for suff in suffixes[nameMap[key][subsuff]]:
+                    for suff in suffixes.suffixes[nameMap[key][subsuff]]:
                         if word.endswith(suff):
                             rezult = True
                             break
@@ -27,25 +28,10 @@ def isName(word):
                 else:
                     rezult = False
             elif (len(word) - len(key) < 3):  # the first form of name
-                for suff in suffixes[nameMap[key][""]]:
+                for suff in suffixes.suffixes[nameMap[key][""]]:
                     if word.endswith(suff):
                         rezult = True
-                        print key, suff
                         break
                     else:
                         rezult = False
     return rezult
-
-def getSuff(name, suffixes):
-    return "kek"
-
-
-print isName("Kj[")
-
-print isName("Володьке")
-print isName("Вованом")
-print isName("Димоном")
-print isName("Петеньку")
-
-
-isName("kek")
