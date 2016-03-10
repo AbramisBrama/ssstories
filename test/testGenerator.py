5507200af3127c521f8d099937bc463b606ed104
Vladimir Golubkov <Vladimir Golubkov>
2016-03-10 22:48:22 +0300

    first version of generator is ready

--- test/testGenerator.py
+++ test/testGenerator.py
@@ -3,8 +3,9 @@ import text.generator
 
 class TestGenerator(unittest.TestCase):
 
-  def test_replace_name(self):
-      self.assertEqual('kek', text.generator.getStory())
+  def test_min_length(self):
+      self.assertGreater(a, b)
+ #     self.assertEqual('kek', text.generator.getStory())
 
 if __name__ == '__main__':
     unittest.main()
